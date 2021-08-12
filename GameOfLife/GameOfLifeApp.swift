692fd508c70aad3b01ce944a8a3726add0290736
RAYCHEV <RAYCHEV@users.noreply.github.com>
2021-08-12 20:51:16 +0100

    Initial project structure

---
+++ GameOfLife/GameOfLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfLifeApp.swift
+//  GameOfLife
+//
+//  Created by Nikolay Raychev on 9.08.21.
+//
+
+import SwiftUI
+
+@main
+struct GameOfLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
